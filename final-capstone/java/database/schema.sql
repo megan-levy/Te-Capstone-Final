145ffc109a7cfbfc4ff6d537ccb3ba1fe2199052
Lee Moeller <armorgroupsniper03@gmail.com>
2021-12-05 22:05:30 -0500

    added lists and items tables

--- final-capstone/java/database/schema.sql
+++ final-capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, groups, CASCADE;
+DROP TABLE IF EXISTS users, groups, lists CASCADE;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -20,17 +20,44 @@ CREATE TABLE users (
 
 CREATE TABLE groups (
  group_id SERIAL,
- group_name varchar(50) NOT NULL,
- username varchar(50) NOT NULL,
+ name varchar(50)UNIQUE NOT NULL,
+ user_id int NOT NULL,
  invite_sent boolean ,
  role varchar(10) NOT NULL,
- joined_on DATE NOT NULL,
+ joined_on DATE DEFAULT CURRENT_TIMESTAMP ,
  CONSTRAINT PK_group_id PRIMARY KEY (group_id),
- CONSTRAINT FK_username FOREIGN KEY (username) REFERENCES users(username) 
+ CONSTRAINT FK_groups_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+
  );
+ 
+ CREATE TABLE lists (
+ list_name varchar(50)UNIQUE NOT NULL,
+ group_id int NOT NULL,
+ group_name varchar(50) UNIQUE ,
+ retail_name varchar(50) UNIQUE,
+ list_claimed boolean,
+ CONSTRAINT PK_lists PRIMARY KEY (list_name),
+  CONSTRAINT FK_lists_group_name FOREIGN KEY (group_name) REFERENCES groups (name)
+ );
+ 
+ CREATE TABLE items (
+ list_item_id varchar(50) NOT NULL,
+ item_name varchar UNIQUE NOT NULL,
+ item_amount int,
+ list_name varchar UNIQUE NOT NULL,
+ date_added DATE DEFAULT CURRENT_TIMESTAMP,
+ user_id int NOT NULL,
+ favorite boolean,
+ coupon boolean,
+ CONSTRAINT PK_items_table PRIMARY KEY (list_item_id),
+ CONSTRAINT FK_items_list_name FOREIGN KEY (list_name) REFERENCES lists(list_name)
+ );
+ 
+
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+
 
 
 COMMIT TRANSACTION;
