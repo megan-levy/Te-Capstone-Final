41d725b15b06e4003ed64a86f9e3503c7a234818
Tristan Guckenberger <tristanguckenberger@pop-os.localdomain>
2021-12-05 19:50:07 -0500

    form styling

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -8,3 +8,103 @@
   </div>
 </template>
 
+<style>
+  html, body {
+    font-family: 'Inter', sans-serif;
+    color: #555b6d;
+  }
+  html,body {
+    height: 100%;
+    margin: 0px;
+  }
+  #app {
+    width: 100%;
+    height: 100%;
+    background-color: #E1E5F2;
+    align-items: center;
+    filter: drop-shadow(0px 4px 4px rgba(221, 221, 221, 0.25));
+  }
+  div.auth-container {
+    width: 100%;
+    height: calc(100% - 18px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  form {
+    /* min-height: 420px; */
+    height: fit-content;
+    min-width: 350px;
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    background-color: #ffffff;
+    border-radius: 6px;
+    filter: drop-shadow(0px 4px 4px rgba(221, 221, 221, 0.25));
+    padding: 25px;
+  }
+  h1, h2, h3 {
+    font-weight: 700;
+    margin-block-end: 0;
+  }
+  h1 {
+    font-size: 22px;
+  }
+  h2 {
+    font-size: 18px;
+  }
+  h3 {
+    font-size: 16px;
+  }
+  hr {
+    background-color: #e1e5f25c;
+    border: none;
+    height: 4px;
+    border-radius: 1px;
+    width: 100%;
+  }
+  label.sr-only {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2rem;
+    font-weight: 500;
+  }
+  label.sr-only span {
+    margin-bottom: 4px;
+  }
+  input[type=text], input[type=password] {
+    /* margin-top: 4px; */
+    padding: 10px;
+    background-color: #E1E5F2;
+    border: none;
+    border-radius: 4px;
+  }
+  input[type=text]:focus-visible, input[type=password]:focus-visible {
+    outline-color: hsl(226deg 40% 70%);
+  }
+  button {
+    padding: 8px;
+    background-color: #E1E5F2;
+    border: none;
+    border-radius: 4px;
+    margin-top: 1rem;
+    font-weight: 600;
+    border: 2px solid transparent;
+    color: #555b6d;
+    transition: all 250ms cubic-bezier(0.23, 1, 0.320, 1);
+  }
+  button:hover {
+    cursor: pointer;
+    border: 2px solid hsl(226deg 40% 70%);
+  }
+  ::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+    font-weight: 600;
+  }
+  a.helper-link {
+    color: #1F7A8C;
+    font-size: .8rem;
+    font-weight: 600;
+  }
+
+</style>
--- final-capstone/vue/src/views/Register.vue
+++ final-capstone/vue/src/views/Register.vue
@@ -1,41 +1,52 @@
 <template>
-  <div id="register" class="text-center">
+  <div id="register" class="text-center auth-container">
     <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
+      <hr>
+      <br>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-      <router-link :to="{ name: 'login' }">Have an account?</router-link>
+      <label for="username" class="sr-only">
+        <span>Username</span>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+      </label>
+      <label for="password" class="sr-only">
+        <span>Password</span>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+      </label>
+      <label for="confirmPassword" class="sr-only">
+        <span>Confirm Password</span>
+        <input
+          type="password"
+          id="confirmPassword"
+          class="form-control"
+          placeholder="Confirm Password"
+          v-model="user.confirmPassword"
+          required
+        />
+      </label>
+      
       <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
+        Sign Up
       </button>
+      <br>
+      <router-link tag="a" class="helper-link" :to="{ name: 'login' }">Already have an account?</router-link>
     </form>
   </div>
 </template>
