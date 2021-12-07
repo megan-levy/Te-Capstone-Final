b0bbcdc74437fc64d3895afe3fd1e2e3925dfc63
Lee Moeller <armorgroupsniper03@gmail.com>
2021-12-07 17:47:55 -0500

    all tables have data added

--- final-capstone/java/database/schema.sql
+++ final-capstone/java/database/schema.sql
@@ -32,9 +32,9 @@ CREATE TABLE groups (
 
  
  CREATE TABLE lists (
- list_id SERIAL NOT NULL,
+ list_id SERIAL ,
  list_name varchar(50)UNIQUE NOT NULL,
- group_id int NOT NULL,
+ group_id int ,
  group_name varchar(50) UNIQUE ,
  retail_name varchar(50) UNIQUE,
  list_claimed boolean,
@@ -43,10 +43,10 @@ CREATE TABLE groups (
  );
  
  CREATE TABLE retailer_store (
- retailer_id SERIAL NOT NULL,
+ retailer_id SERIAL,
  retail_name varchar(50) UNIQUE,
  rewards varchar(100) UNIQUE,
- group_id int NOT NULL,
+ group_id int,
  CONSTRAINT PK_retailer_store PRIMARY KEY (retailer_id),
  CONSTRAINT FK_retailer_list FOREIGN KEY (retail_name) REFERENCES lists(retail_name),
  CONSTRAINT FK_one_more_key FOREIGN KEY (group_id) REFERENCES groups (group_id)
@@ -55,12 +55,12 @@ CREATE TABLE groups (
  
  
  CREATE TABLE items (
- list_item_id varchar(50) NOT NULL,
+ list_item_id varchar(50) NOT NULL ,
  item_name varchar UNIQUE NOT NULL,
  item_amount int,
  list_name varchar UNIQUE NOT NULL,
  date_added DATE DEFAULT CURRENT_TIMESTAMP,
- user_id int NOT NULL,
+ user_id int,
  favorite boolean,
  rewards_id varchar(100) UNIQUE,
  CONSTRAINT PK_items_table PRIMARY KEY (list_item_id),
@@ -73,6 +73,10 @@ CREATE TABLE groups (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 INSERT INTO groups (name,invite_sent,role) VALUES ('Administrator','true', 'ROLE_ADMIN');
+INSERT INTO groups (name, invite_sent, role) VALUES ('Mickey_Mouse', 'true', 'ROLE_USER');
+INSERT INTO lists (list_name, group_name, retail_name, list_claimed) VALUES ('Magic', 'Mickey_Mouse', 'Kroger', 'false');
+INSERT INTO retailer_store (retail_name, rewards) VALUES ('Kroger', '5133161752');
+INSERT INTO items (list_item_id,item_name, item_amount, list_name, favorite, rewards_id) VALUES ('1', 'Spaghetti_squash', '1', 'Magic','false', '5133161752');
 
 
 COMMIT TRANSACTION;
