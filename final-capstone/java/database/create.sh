0d6dffec8c8784abbebcfdb3078d503e1dd53d47
Tristan Guckenberger <tristan@gcans.com>
2021-12-10 22:20:26 +0000

    create.sh edited online with Bitbucket

--- final-capstone/java/database/create.sh
+++ final-capstone/java/database/create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
 DATABASE=final_capstone
-USR=tristanguckenberger
-PGPASSWORD=Fpw26gcg!!
+USR=final_capstone
+PGPASSWORD=postgres1
 psql -U $USR -f "$BASEDIR/dropdb.sql" &&
 createdb -U $USR $DATABASE &&
 psql -U $USR -d $DATABASE -f "$BASEDIR/schema.sql" &&
