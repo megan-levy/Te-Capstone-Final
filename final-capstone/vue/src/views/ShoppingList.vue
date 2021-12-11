0f8153121baa36f3979d5a35c038d375caed0dd3
Megan Levy <megan.levy3@gmail.com>
2021-12-10 22:28:00 -0500

    CSS updated

--- final-capstone/vue/src/views/ShoppingList.vue
+++ final-capstone/vue/src/views/ShoppingList.vue
@@ -2,15 +2,39 @@
   <div id="shopping-list" class="shopping-list">
       <h1>{{listName}}</h1>
       <p>{{listDescription}}</p>
-      <div>
-        <ul>
+      <div class="grocery-list">
+        <ul class="vertical-list" id="items-listed">
           <li>  
+           <div>
              <h3>
                Lots of wine
              </h3>
+            <span>Quantity: </span>
+            </div>
           </li>
           <li>  
+            <div>
              <h3>Pie</h3>
+             <span>Quantity: </span>
+             </div>
+          </li>
+          <li> 
+            <div> 
+             <h3>pizza</h3>
+             <span>Quantity: </span>
+             </div>
+          </li>
+          <li> 
+            <div> 
+             <h3>carrots</h3>
+             <span>Quantity: </span>
+             </div>
+          </li>
+          <li>  
+            <div>
+             <h3>eggs</h3>
+             <span>Quantity: </span>
+             </div>
           </li>
         </ul>
       </div>
@@ -50,5 +74,39 @@ export default {
   border-radius: 6px;
   filter: drop-shadow(0px 4px 4px rgba(221, 221, 221, 0.25));
   padding: 25px;
+}
+
+#items-listed {
+  display: grid;
+  grid-template-columns:repeat(auto-fit, minmax(250px, 1fr));
+  width: 100%;
+  padding-inline-start: 0px;
+}
+
+h1 {
+  padding: 15px;
+}
+
+p {
+  margin: 20px;
+}
+
+h3 {
+  font-size: 18px;
+  margin-block-start: 0;
+}
+
+li span {
+  font-size: 14px;
+  padding-top: 20px;
+}
+li {
+  background-color: #e1e5f2;
+  padding: 15px;
+  margin: 10px;
+  border-radius: 6px;
+  list-style-type: none;
+  display: flex;
+  
 }
 </style>
